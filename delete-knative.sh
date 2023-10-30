8c6d6faf200164a2f5ca6f0d53df144884b41f35
sonjoyp <sonjoy.k.paul@gmail.com>
2023-10-29 21:55:17 -0400

    basic for knative

---
+++ delete-knative.sh
@@ -0,0 +1,32 @@
+kubectl delete -f https://github.com/knative/serving/releases/download/knative-v1.11.1/serving-hpa.yaml
+kubectl delete -f https://github.com/knative/net-certmanager/releases/download/knative-v1.11.1/release.yaml
+kubectl delete -f https://github.com/knative/net-http01/releases/download/knative-v1.11.1/release.yaml
+
+
+#Istio
+kubectl delete -f https://github.com/knative/net-istio/releases/download/knative-v1.11.0/net-istio.yaml
+kubectl delete -f https://github.com/knative/net-istio/releases/download/knative-v1.11.0/istio.yaml
+
+#Magic DNS (sslip.io)
+kubectl delete -f https://github.com/knative/serving/releases/download/knative-v1.11.1/serving-default-domain.yaml
+
+#Serving Component
+kubectl delete -f https://storage.googleapis.com/knative-nightly/serving/latest/serving-core.yaml
+kubectl delete -f https://storage.googleapis.com/knative-nightly/serving/latest/serving-crds.yaml
+
+
+#Kafka
+kubectl delete -f https://github.com/knative/eventing/releases/download/knative-v1.11.4/eventing-sugar-controller.yaml
+kubectl delete -f https://github.com/knative-sandbox/eventing-kafka-broker/releases/download/knative-v1.11.6/eventing-kafka-source.yaml
+
+#Broker
+kubectl delete -f https://github.com/knative-sandbox/eventing-kafka-broker/releases/download/knative-v1.11.6/eventing-kafka-broker.yaml
+kubectl delete -f https://github.com/knative-sandbox/eventing-kafka-broker/releases/download/knative-v1.11.6/eventing-kafka-controller.yaml
+
+#Kafka Channel
+kubectl delete -f https://github.com/knative-sandbox/eventing-kafka-broker/releases/download/knative-v1.11.6/eventing-kafka-channel.yaml
+
+
+#Knative Eventing
+kubectl delete -f https://storage.googleapis.com/knative-nightly/eventing/latest/eventing-core.yaml
+kubectl delete -f https://storage.googleapis.com/knative-nightly/eventing/latest/eventing-crds.yaml
---
+++ deploy-knative.sh
@@ -0,0 +1,9 @@
+sudo apt-get install jq
+kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.11.1/serving-crds.yaml
+kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.11.1/serving-core.yaml
+kubectl apply -l knative.dev/crd-install=true -f https://github.com/knative/net-istio/releases/download/knative-v1.11.0/istio.yaml
+kubectl apply -f https://github.com/knative/net-istio/releases/download/knative-v1.11.0/istio.yaml
+kubectl apply -f https://github.com/knative/net-istio/releases/download/knative-v1.11.0/net-istio.yaml
+kubectl --namespace istio-system get service istio-ingressgateway
+#kubectl get pods -n knative-serving
+#kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.11.1/serving-default-domain.yaml
